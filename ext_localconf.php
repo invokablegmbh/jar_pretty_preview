41df84c487cb639ccce84cb1c773c3196fda89ba
Container 11823 cc-11823@jcdn.de <script@jcdn.de>
2022-10-14 11:43:00 +0200

    Added devtrolls fix

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 call_user_func(function () {
     $cachingConfigurations = &$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'];
     if (is_array($cachingConfigurations)) {
-        if (!is_array($cachingConfigurations['pretty_preview_content'])) {
+        if (!array_key_exists('pretty_preview_content', $cachingConfigurations)) {
             $cachingConfigurations['pretty_preview_content']  = [
                 'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
                 'backend' => \TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend::class,
